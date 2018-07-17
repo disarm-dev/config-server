56ca7e2a2ca0a0a853b76219b36b8175c1f478cc
Sihle Mbhamali <mbhamalimosaic@gmail.com>
2018-07-17 14:00:13 +0200

    Possible hook

---
+++ api/hooks/auth/index.js
@@ -0,0 +1,30 @@
+module.exports = function auth(sails) {
+  sails.log.info('Auth hook loaded')
+  return {
+    name:'Auth Hook', 
+    initialize: function(cb) {
+        //Initialise source of permissions
+        return cb();
+    },
+    routes: {
+      before: {
+        'GET /*': function (req, res, next) {
+          let user = req.user;
+          let resource = req.path;
+
+          if(can(user, 'View', resource)){
+            sails.log.info('Geting from ',resource)
+            return next();
+          }else{
+            return ne
+          }
+          
+        }
+      }
+    }
+  }
+}
+
+function can(user, action, resource){
+  return true
+}
