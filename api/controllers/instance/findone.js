c0207a0bd78d479a21425235984cebc4659f09f2
Unknown <mbhamalimosaic@gmail.com>
2018-07-31 07:58:55 +0200

    FindOne permissions and edited test

--- api/controllers/instance/findone.js
+++ api/controllers/instance/findone.js
@@ -27,6 +27,15 @@ module.exports = {
 
 
   fn: async function (inputs, exits) {
+    let {api_key} = this.req.headers
+    let {user_id} = await Session.findOne({api_key})
+    let instance_id = inputs.id
+
+    let can = await sails.helpers.can.with({user_id,instance_id,value:'read'})
+    
+    if(!can){
+      return exits.fail('Permission denied')
+    }
     const instance = await Instance.findOne({id: inputs.id})
     return exits.success(instance)
   }
