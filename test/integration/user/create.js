f455d6768954bf9c941669884cd49f486bad1d7d
mosaic141688 <mbhamalimosaic@gmail.com>
2018-08-23 09:39:15 +0200

    Create user tests

--- test/integration/user/create.js
+++ test/integration/user/create.js
@@ -29,7 +29,37 @@ test('can create a user, as super admin', async t => {
   t.is(res.status, 200)
 })
 
-test.todo('only users with correct permission can create user')
+test('POST /users fails to create a user as admin', async t => {
+  const user = await sails.models.user.create({ username: 'nd', encrypted_password: '123' }).fetch()
+  await sails.models.session.create({ user_id: user.id, api_key: 'api_key_123' })
+  const instance = await sails.models.session.create({name:'New Instance'}).fetch()
+
+  await sails.helpers.addPermission.with({user_id:user.id, instance_id:instance.id, value:'admin'})
+
+  const res = await supertest(sails.hooks.http.app)
+    .post(`/users`)
+    .send({username: 'js', password: 'malaria123'})
+    .set('api_key', 'api_key_123')
+
+  t.is(res.status, 401)
+})
+
+
+test('POST /users fails to create a user as any user', async t => {
+  const user = await sails.models.user.create({ username: 'nd', encrypted_password: '123' }).fetch()
+  await sails.models.session.create({ user_id: user.id, api_key: 'api_key_123' })
+
+  const res = await supertest(sails.hooks.http.app)
+    .post(`/users`)
+    .send({username: 'js', password: 'malaria123'})
+    .set('api_key', 'api_key_123')
+
+  t.is(res.status, 401)
+})
+
+
+
+
 
 test('invalid username returns error', async t => {
   const user = await sails.models.user.create({ username: 'nd', encrypted_password: '123' }).fetch()
