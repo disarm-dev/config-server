b21dba19754c8bf1a125255a9d63d8cfb100dfbc
mosaic141688 <mbhamalimosaic@gmail.com>
2018-08-29 16:28:54 +0200

    Enable all instances for super admin

--- api/controllers/instance/find.js
+++ api/controllers/instance/find.js
@@ -29,10 +29,19 @@ module.exports = {
 
     let can = await sails.helpers.can.with({req:this.req, resource:'user', action:'read', user_id:inputs.user_id})
 
+
     if(can){
       let user = await User.findOne({id:inputs.user_id}).populate('instances')
-      console.log('Instances', user)
-      return exits.success(user.instances)
+      let instances = user.instances;
+      //Super admin gets all instances
+
+      const is_super_admin = await Permission.findOne({id:inputs.user_id,value:'super-admin'})
+      if(is_super_admin){
+        const all_instances = await Instance.find()
+        instances = all_instances;
+      }
+
+      return exits.success(instances)
     }
    return exits.fail('Permission denied')
   }
