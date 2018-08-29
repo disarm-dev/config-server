27a1ee460bb8fe82407f9538ff3ec9d164317167
mosaic141688 <mbhamalimosaic@gmail.com>
2018-08-29 15:32:24 +0200

    user associations work

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt')
+const cjson = require('cjson')
 /**
  * Bootstrap
  * (sails.config.bootstrap)
@@ -12,15 +13,90 @@ const bcrypt = require('bcrypt')
 
 
 module.exports.bootstrap = async function (done) {
-
   if (await User.count() > 0) {
     return done();
   }
+  try{
 
-  bcrypt.hash('Malaria', 10, async function (err, hash) {
-    let {id} = await User.create({username: 'groot', encrypted_password: hash}).fetch();
-    await sails.helpers.addPermission.with({user_id: id, value: 'super-admin'})
-    return done();
+  
+
+  const users_json = cjson.load('./fixtures/users.json')
+  const permissions = cjson.load('./fixtures/permissions.json')
+
+  const bwa_instance_config = cjson.load('./fixtures/bwa/config.json')
+  const nam_instance_config = cjson.load('./fixtures/nam/config.json')
+  const bwa_clusters = cjson.load('./fixtures/bwa/clusters.json')
+  const bwa_districrts = cjson.load('./fixtures/bwa/districts.json')
+  const bwa_villages = cjson.load('./fixtures/bwa/villages.json')
+  const nam_constituencies = cjson.load('./fixtures/nam/constituencies.json')
+  const nam_villages = cjson.load('./fixtures/nam/villages.json')
+  
+  const user_ids = [];
+
+  for(let i = 0; i<users_json.length; i++){
+    user_ids.push(await create_user(users_json[i]))
+    console.log('User ids',user_ids)
+  }
+
+  await users_json.forEach(async user => {
+   
   });
 
+
+  const nam_instance = await create_instance(nam_instance_config.instance.title)
+  const nam_config = await create_config({version:'0.0.1', lob:nam_instance_config})
+  await Instance.addToCollection(nam_instance.id,'users',user_ids)
+  await Instance.addToCollection(nam_instance.id, 'instanceconfigs',nam_config.id)
+
+  user_ids.forEach(async user_id => {
+    permissions.forEach(async value => {
+      const perm = await sails.helpers.addPermission.with({user_id, value , instance_id: nam_instance.id})
+      
+    })
+  })
+
+  const bwa_instance = await create_instance(bwa_instance_config.instance.title) 
+  const bwa_config = await create_config({version:'0.0.1', lob:bwa_instance_config})
+  await Instance.addToCollection(bwa_instance.id,'users',user_ids)
+  await Instance.addToCollection(bwa_instance.id, 'instanceconfigs',bwa_config.id)
+
+  user_ids.forEach(user_id => {
+    permissions.forEach(async value => {
+      await sails.helpers.addPermission.with({user_id, value , instance_id: bwa_instance.id})
+    })
+  })
+
+  const nw =  await Instance.find().populate('users')
+  console.log('Seting up pemrission',nw)
+
+  return done()
+}
+catch(e){
+  console.log(e)
+  return 
+}
+
 };
+
+async function create_user({password, username,role,permissions}){
+  return new Promise((resolve,reject)=>{
+    bcrypt.hash(password, 10, async function (err, hash) {
+      let {id} = await User.create({username, encrypted_password: hash}).fetch();
+      await sails.helpers.addPermission.with({user_id: id, value: role})
+      resolve(id)
+    });
+  })
+}
+
+async function create_instance(name){
+  return await Instance.create({name}).fetch();
+}
+
+async function create_config({lob}){
+  return await InstanceConfig.create({version:1,lob}).fetch();
+}
+
+
+async function create_geodata_layer({name,version,file}){
+  return await LargeFile.create({name,version,file}).fetch()
+}
